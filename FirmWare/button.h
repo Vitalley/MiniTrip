73075ffff09405713c3910853187c77e6bb0476b
Vitaliy <devildie@inbox.ru>
2023-08-25 12:17:24 +0300

    Delete FirmWare directory

--- FirmWare/button.h
+++
@@ -1,3 +0,0 @@
-#include <xc.h>
-
-char GetButton(void);
--- FirmWare/config.h
+++
@@ -1,8 +0,0 @@
-#include <xc.h>
-#define _XTAL_FREQ 32000000
-#define I2C_SPEED 100
-#define ft_mult 570
-#define ft_sdd 370
-#define array_size_voltage 8
-#define array_size_fuel 64
-#define impulse_per_litre 22500
