5325285a39bd1955d7be6ce826d064842171b315
Vitaliy <devildie@inbox.ru>
2023-08-25 12:18:50 +0300

    Main
    Main MiniTrip

---
+++ FIRMWARE/EEPROM.h
@@ -0,0 +1,7 @@
+unsigned long int erd(unsigned char eeadr);
+void ewr(unsigned long int dt,unsigned char eeadr);
+void EEREADDATA(void);
+void EEWRITEFATA(void);
+void EETRIPRESET(void);
+void EETO1RESET(void);
+void EETO2RESET(void);
